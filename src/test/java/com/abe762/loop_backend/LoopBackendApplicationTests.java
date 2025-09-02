084be855de54627fb7fbf5045b59cedd56714d4c
Ahmad Zaenal Abidin <ahmadabidin762.dev@gmail.com>
2025-09-02 21:35:49 +0700

    first commit

---
+++ src/test/java/com/abe762/loop_backend/LoopBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abe762.loop_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoopBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
